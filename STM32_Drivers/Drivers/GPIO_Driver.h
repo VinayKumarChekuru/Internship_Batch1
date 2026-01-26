260aeb328da332606e0457a51d4efab3a98349cb
upendar1419 <vilasagarapuupendar81189@gmail.com>
2026-01-26 11:22:54 +0400

    GPIO_Deint
    Implemented the de-initialization of the GPIO peripheral clock, and completed the class code up to 25/01/2026.
    Changed files:  main.c, GPIO_Driver.h and GPIO_Driver.c

--- STM32_Drivers/Drivers/GPIO_Driver.h
+++ STM32_Drivers/Drivers/GPIO_Driver.h
@@ -9,16 +9,36 @@
 #define GPIO_DRIVER_H_
 
 #include"STM32F051.h"
+/*
+ * Modes of GPIO
+ */
+#define GPIO_MODE_IN		0
+#define GPIO_MODE_OUT		1
+#define GPIO_MODE_ALTFN		2
+#define GPIO_MODE_ANALOG	3
 
+/*
+ * GPIO output type
+ */
+#define GPIO_PUSHPULL		0
+#define GPIO_OPENDRAIN		1
+
+/*
+ * GPIO Output Speed
+ */
+#define GPIO_SPEED_LOW		0	// 0 2 ALSO LOWSPEED
+#define GPIO_SPEED_MED	1
+#define GPIO_SPEED_HIGH		3
 
 /*
  * GPIO Configuration structure
  */
-
+/*
 	__vo ui32 MODER;
 	__vo ui32 OTYPER;
 	__vo ui32 OSPEEDR;
 	__vo ui32 PUPDR;
+*/
 typedef struct
 {
 	uint8_t Pin_Num;   //Pin Number of a Port
@@ -29,11 +49,18 @@ typedef struct
 
 }GPIO_Config_t;
 
-
-
+/*
+ * GPIO Handler structure
+ */
+typedef struct
+{
+	GPIO_RegDef_t *PGPIOx;//pointer variable so access data ->(arrow operator)
+	GPIO_Config_t GPIO_Config;//variable so access data .(dot operator)
+}GPIO_Handler_t;
 
 
 void GPIO_Clk_EnorDi(GPIO_RegDef_t *pGPIOx,uint8_t EnorDi);
-void GPIO_Init(GPIO_RegDef_t *PGPIOx,GPIO_Config_t GPIO_Config);
+void GPIO_Init(GPIO_Handler_t GPIO_Handler);
+void GPIO_Deint(GPIO_RegDef_t *pGPIOx);
 
 #endif /* GPIO_DRIVER_H_ */
--- STM32_Drivers/Src/main.c
+++ STM32_Drivers/Src/main.c
@@ -17,7 +17,7 @@
  */
 
 #include <stdint.h>
-
+#include<stdio.h>
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
@@ -25,7 +25,7 @@
 int main(void)
 {
     /* Loop forever */
-	// hai hai
+	printf("Hello world");
 	for(;;);
 
 }
